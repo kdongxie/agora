278eb0e36758f676b4e0630523743190043c5613
DongSup Kim <kdongxie@naver.com>
2019-06-04 17:34:26 +0900

    first commit

---
+++ intro/ordinarypeople/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrdinarypeopleConfig(AppConfig):
+    name = 'ordinarypeople'
---
+++ intro/politics/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+import random
+import math
+from datetime import datetime
+import json
+import requests
+
+# Create your views here.
+def index(request):
+    return render(request, 'politics/index.html')
